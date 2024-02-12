25f4bfbd7a613af29c07090d64cc55450df262f1
matteomedori <matteo.medori.97@gmail.com>
2024-02-12 14:53:23 +0100

    first commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $types = Type::all();
+        return view('admin.projects.create', compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        $data = $request->validated();
+
+        $newproject = new Project();
+        $newproject->fill($data);
+        $newproject->slug = Str::of($newproject->title)->slug('-');
+        $newproject->save();
+
+        return redirect()->route('admin.projects.index')->with('messages', "Progetto '$newproject->title' aggiunto correttamente");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::all();
+
+        return view('admin.projects.edit', compact('project', 'types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $data = $request->validated();
+
+        $project->update($data);
+        $project->slug = Str::of($project->title)->slug('-');
+        $project->save();
+
+        return redirect()->route('admin.projects.index')->with('messages', "Progetto '$project->title' modificato correttamente");;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project)
+    {
+        $title = $project->title;
+        $project->delete();
+        return redirect()->route('admin.projects.index')->with('messages', "Progetto '$title' cancellato correttamente");
+    }
+}
