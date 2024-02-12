c1ccb7cb34d46f509def2b1a7924e313d921de49
matteomedori <matteo.medori.97@gmail.com>
2024-02-12 17:02:06 +0100

    feat: lista progetti su dettagli tecnologia

--- resources/views/admin/technologies/show.blade.php
+++ resources/views/admin/technologies/show.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <h2 class="my-3">{{ $technology->name }}</h2>
-    <p>slug: {{ $technology->slug }}</p>
+    <p>Slug: {{ $technology->slug }}</p>
+    <div>Progetti che la utilizzano:
+        <ul>
+            @foreach ($technology->projects as $project)
+                <li>{{ $project->title }}</li>
+            @endforeach
+        </ul>
+    </div>
     <a href="{{ route('admin.technologies.index') }}" class="btn btn-info text-light">Torna alla lista delle tecnologie</a>
 @endsection
