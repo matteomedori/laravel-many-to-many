bb10fb80c2bf9a3524f0a48669059556dfba63b4
matteomedori <matteo.medori.97@gmail.com>
2024-02-13 15:09:47 +0100

    feat: aggiunta lista progetti su dettagli type

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <h2 class="my-3">{{ $type->name }}</h2>
-    <p>slug: {{ $type->slug }}</p>
+    <p>Slug: {{ $type->slug }}</p>
+    <div>Progetti di questo tipo:
+        <ul>
+            @foreach ($type->projects as $project)
+                <li>{{ $project->title }}</li>
+            @endforeach
+        </ul>
+    </div>
     <a href="{{ route('admin.types.index') }}" class="btn btn-info text-light">Torna alla lista delle tipologie</a>
 @endsection
